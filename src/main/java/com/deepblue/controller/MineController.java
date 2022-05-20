2a1bb87b3c7b682c2e65e9a7dbf3c8eec0fbe985
yangjianwei <2017585616@qq.com>
2022-05-20 15:26:54 +0800

    Nginx实战_常用内置变量

--- src/main/java/com/deepblue/controller/MineController.java
+++ src/main/java/com/deepblue/controller/MineController.java
@@ -1,5 +1,7 @@
 package com.deepblue.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MineController {
 
+    Logger logger = LoggerFactory.getLogger(MineController.class);
+
     @RequestMapping("sayHello")
     public String sayHello(String username) {
+
+        logger.info("say hello method invoke, username is " + username);
 
         return "Hello " + username;
 
